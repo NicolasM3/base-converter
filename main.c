566af35402b63885967bb2ed74488a6e16fbc09c
NicolasM3 <niicck992@gmail.com>
2020-10-23 21:52:22 -0300

    Achar virgula

--- main.c
+++ main.c
@@ -4,33 +4,60 @@
 
 #define qtdDigits 36
 
-char digits[qtdDigits] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char digits[qtdDigits] =
+  { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
+  'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+  'u', 'v', 'w', 'x',
+  'y', 'z'
+};
 
-void latterToNumber(char latter)
+void
+letterToNumber (char letter)
 {
-    for(int i = 0; i < qtdDigits; i++)
+  for (int i = 0; i < qtdDigits; i++)
     {
-        if(latter == digits[i])
-        {
-            printf("%c", digits[i]);
-        }
+      if (letter == digits[i])
+	{
+	  printf ("%c", digits[i]);
+	}
     }
 }
 
-char numberToLatter(int number)
+char
+numberToletter (int number)
 {
-    return digits[number]; 
+  return digits[number];
 }
 
-int main()
+int
+findComma (char number[])
 {
-    char input_number[10];
-    int primary_base, second_base;
-    gets(input_number);
-    scanf("%d %d", &primary_base, &second_base);
-    
-    return 0;
+  signed int found = -1;
+  for (int i = 0; i < 10; i++)
+    {
+      if (number[i] == '\\')
+	break;
+      if (number[i] == ',')
+	{
+	  found = i;
+	  break;
+	}
+
+    }
+
+  return found;
 }
 
+int
+main ()
+{
+  char input_number[10];
+  int primary_base, second_base;
+  fgets (input_number, 10, stdin);
+  scanf ("%d %d", &primary_base, &second_base);
 
+  printf ("%d", findComma (input_number));
+
+  return 0;
+}
 
